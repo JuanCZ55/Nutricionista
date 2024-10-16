1cda069ea13b6f94b7dc378b783cdb36e5226a00
JuanCZ55 <juancruzzegarra@gmail.com>
2024-10-16 20:15:24 -0300

    se hizo la conexion

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -1,4 +1,37 @@
 package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class Conexion {
-    
+        private static Conexion connection = null;
+
+    private Conexion() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Error al cargar Driver");
+
+        }
+    }
+
+    public static Connection getConexion() {
+        Connection con = null;
+
+        if (connection == null) {
+            connection = new Conexion();
+        }
+
+        try {
+            con = DriverManager.getConnection("jdbc:mariadb://localhost/universidadulp_trasversal", "root", "");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error: error al conectarse a la base de datos");
+        }
+
+        return con;
+    }
+
 }
