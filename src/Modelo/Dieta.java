6315279836de6956f6ee5e046ad440260347ce87
JuanCZ55 <juancruzzegarra@gmail.com>
2024-10-16 18:19:33 -0300

    Initial commit

---
+++ src/Modelo/Dieta.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author juanz
+ */
+public class Dieta {
+
+    private int idDieta;
+    private String nombre;
+    private Paciente paciente;
+    private LocalDate fechaInicial;
+    private LocalDate fechaFinal;
+    private Double pesoInicial;
+    private Double pesoFinal;
+
+    public Dieta() {
+    }
+
+    public Dieta(int idDieta, String nombre, Paciente paciente, LocalDate fechaInicial, LocalDate fechaFinal, Double pesoInicial, Double pesoFinal) {
+        this.idDieta = idDieta;
+        this.nombre = nombre;
+        this.paciente = paciente;
+        this.fechaInicial = fechaInicial;
+        this.fechaFinal = fechaFinal;
+        this.pesoInicial = pesoInicial;
+        this.pesoFinal = pesoFinal;
+    }
+
+    public Dieta(String nombre, Paciente paciente, LocalDate fechaInicial, LocalDate fechaFinal, Double pesoInicial, Double pesoFinal) {
+        this.nombre = nombre;
+        this.paciente = paciente;
+        this.fechaInicial = fechaInicial;
+        this.fechaFinal = fechaFinal;
+        this.pesoInicial = pesoInicial;
+        this.pesoFinal = pesoFinal;
+    }
+
+    public int getIdDieta() {
+        return idDieta;
+    }
+
+    public void setIdDieta(int idDieta) {
+        this.idDieta = idDieta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public LocalDate getFechaInicial() {
+        return fechaInicial;
+    }
+
+    public void setFechaInicial(LocalDate fechaInicial) {
+        this.fechaInicial = fechaInicial;
+    }
+
+    public LocalDate getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(LocalDate fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public Double getPesoInicial() {
+        return pesoInicial;
+    }
+
+    public void setPesoInicial(Double pesoInicial) {
+        this.pesoInicial = pesoInicial;
+    }
+
+    public Double getPesoFinal() {
+        return pesoFinal;
+    }
+
+    public void setPesoFinal(Double pesoFinal) {
+        this.pesoFinal = pesoFinal;
+    }
+
+}
